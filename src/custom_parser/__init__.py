6da16a11a89b4468fe4b8b664dde0ec3f36a6721
Diogo Pacheco <100862257+dpacheconr@users.noreply.github.com>
2023-11-14 09:27:00 +0000

    debug: span timestamps

--- src/custom_parser/__init__.py
+++ src/custom_parser/__init__.py
@@ -47,8 +47,10 @@ def parse_attributes(obj,att_to_drop):
     #         print("Unable to parse GHA_ATTRIBUTES_DROP, check your configuration")
 
     for attribute in obj:
-        print(attribute)
         attribute_name = str(attribute).lower()
+        if attribute_name.endswith("_at"):
+            print(attribute_name)
+        
         if attribute_name not in attributes_to_drop:
             if do_parse(obj[attribute]):
                 if type(obj[attribute]) is dict:
